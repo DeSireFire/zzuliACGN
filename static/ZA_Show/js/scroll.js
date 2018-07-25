887ed8b8f024967d86bed3c2ea28f0a4cc2fd100
DeSireFire <1025212779@qq.com>
2018-07-26 00:28:32 +0800

    注释部分调试信息，修改滚动触发的分辨率。

--- static/ZA_Show/js/scroll.js
+++ static/ZA_Show/js/scroll.js
@@ -27,17 +27,18 @@
             var $currentWindow = $(window);
             //当前窗口的高度
             var windowHeight = $currentWindow.height();
-            console.log("current widow height is " + windowHeight);
+            //console.log("current widow height is " + windowHeight);
             //当前滚动条从上往下滚动的距离
             var scrollTop = $currentWindow.scrollTop();
-            console.log("current scrollOffset is " + scrollTop);
+            //console.log("current scrollOffset is " + scrollTop);
             //当前文档的高度
             var docHeight = $(document).height();
-            console.log("current docHeight is " + docHeight);
+            //console.log("current docHeight is " + docHeight);
  
             //当 滚动条距底部的距离 + 滚动条滚动的距离 >= 文档的高度 - 窗口的高度
             //换句话说：（滚动条滚动的距离 + 窗口的高度 = 文档的高度）  这个是基本的公式
-            if ((BOTTOM_OFFSET + scrollTop) >= docHeight - windowHeight && pos != MAX_POS) {
+
+            if ((BOTTOM_OFFSET + scrollTop) >= docHeight - windowHeight - 1 && pos != MAX_POS) {
                 showItems();
             }
         });
