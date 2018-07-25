1a4c72926267e12846946247a98f5671b800a274
DeSireFire <1025212779@qq.com>
2018-07-26 00:58:49 +0800

    应用模块调整，新增show模块，专门用于招新维护页。 fix模块名错误。

--- ZA_Show/urls.py
+++ ZA_Show/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path,include
-from ZA_Index import views
+from . import views
 
 
 urlpatterns = [
-    path('', views.ZA_show),
+    path('', views.ZA_Show),
     #此处设置为首页，以前写法是'^$',新版本不再使用^、$，只需要‘’就可以
     # path('index/', include('ZA_Index.urls')),
 ]
--- ZA_Show/views.py
+++ ZA_Show/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 
 # 网站招新展示页
-def ZA_show(request):
+def ZA_Show(request):
     return render(request,'ZA_Show/ZA_show.html')
