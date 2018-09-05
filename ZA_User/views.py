f06ad7e8df5ecb8a714c74e91f94da9876060e87
DeSireFire <1025212779@qq.com>
2018-09-05 23:29:39 +0800

    完成登录js和初步完成登录视图。

--- ZA_User/views.py
+++ ZA_User/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from .models import *
 from django.http import HttpResponseRedirect,JsonResponse
-from hashlib import sha1
+from hashlib import sha256
 from django.core.paginator import Paginator,Page
 from datetime import datetime
 import re
@@ -59,17 +59,17 @@ def Register_Re(keyName,keyValue):
     }
     pattern = re.compile(ResDict.get(keyName))
     retemp = pattern.match(keyValue)
-    print(retemp.group())
+    # print(retemp.group())
     if retemp:
         return True
     else:
         return False
 
 
- # 用户注册
+ # 用户注册处理
 def Register_handle(request):
 
-    print(request.POST)
+
     if 'user_name' in request.GET and request.GET['user_name']:  # 获得用户输入值
         q = request.GET.get('user_name')
         print(q)
@@ -95,7 +95,7 @@ def Register_handle(request):
         return redirect('/user/register/')
     else:
         #加密
-        Encipherment = sha1()
+        Encipherment = sha256()
         Encipherment.update(New_ZA_User_pwd.encode('utf-8'))
         Up_Password_Encipherment = Encipherment.hexdigest()
 
@@ -116,18 +116,94 @@ def Register_handle(request):
         NewUser.save()
     return redirect('/user/login/')
 
-# 用户登录页面
+
+# 用户登录跳转
 def Login(request):
 
     context = {'title': '用户登录'}
     return  render(request,'ZA_User/Login.html',context)
 
+# 用户登录处理
 def Login_Handler(request):
 
-    if 'Email' in request.GET and request.GET['Email']:  # 获得用户输入值
-        New_ZA_User_Email = request.POST.get('Email')
-        #电子邮箱
-        New_ZA_User_pwd = request.POST.get('pwd')
-        #用户密码
-    context = {'title': '用户成功！'}
-    return  render(request,'ZA_User/Login.html',context)
+    # {'password': ['123qwe'], 'user_name': ['1025212779@qq.com'], 'rememberme': ['on'],
+    #  'csrfmiddlewaretoken': ['O8nwHR9211S1Xw0jbWLtsr4YHu2kq7V4RpRPUwnGxkSZZgM1uj7yU57bC1ILjxrM']}
+
+    print(request.POST)
+
+    New_ZA_User_Temp = request.POST.get('user_name')
+
+    # 判断使用邮箱登录还是用户名登录
+    if Register_Re("ZA_User_Email", New_ZA_User_Temp):
+        New_ZA_User_Email = New_ZA_User_Temp
+        print("re:%s"%New_ZA_User_Email)
+        old_users = ZA_UserInfo.objects.filter(ZA_User_Email=New_ZA_User_Email)  # 从后端获取用户名
+
+    elif Register_Re("ZA_User_Name", New_ZA_User_Temp):
+        New_ZA_User_name = New_ZA_User_Temp
+        print("re:%s" % New_ZA_User_name)
+        old_users = ZA_UserInfo.objects.filter(ZA_User_Name=New_ZA_User_name)  # 从后端获取邮箱
+    else:
+        context = {'title': '用户登录失败！'}
+        return render(request, 'ZA_User/Login.html', context)
+
+    # 用户密码
+    New_ZA_User_pwd = request.POST.get('password')
+
+
+    # 登录状态记忆
+    RememberMe = "off"
+    if 'rememberme' in request.POST and request.POST['rememberme']:  # 获得用户输入值
+        RememberMe = request.POST.get('rememberme')
+        print(RememberMe)
+
+    print("Tmp:%s" % New_ZA_User_Temp)
+    print("pwd:%s" % New_ZA_User_pwd)
+    # print("old_users:%s" % old_users[0])
+    # print("old_users:%s" % old_users[0].ZA_User_ID)
+    # print("old_users:%s" % old_users[0].ZA_User_Name)
+    # print("old_users:%s" % old_users[0].ZA_User_Email)
+    # print("CK_URL:%s" % request.COOKIES)
+    # print("CK_URL:%s" % request.COOKIES.get("url"))
+
+    if len(old_users) == 1:
+        print("old_users is 1")
+        # 获取到用户名
+        # 验证密码
+        # 我就是要把变量名写得让你看起来头皮发麻！2333
+        encipherment = sha256()
+        encipherment.update(New_ZA_User_pwd.encode('utf-8'))
+        ZA_User_Password_encipherment = encipherment.hexdigest()
+        if ZA_User_Password_encipherment==old_users[0].ZA_User_Password:
+
+            url = request.COOKIES.get('url','/')
+            cookiesWarn = HttpResponseRedirect(url)
+            # 成功后删除转向地址，防止以后直接登录造成的转向
+            cookiesWarn.set_cookie('url','',max_age=-1)
+            # 记住用户名
+            if RememberMe == "on":
+                print("保持登录状态！")
+                # 用户名以utf8编码以保证cookies不报错，使用时用.decode("utf-8")解码
+                cookiesWarn.set_cookie("user_name",old_users[0].ZA_User_Name.encode('utf-8').decode())
+            else:
+                print("未勾选登录状态记忆！")
+                cookiesWarn.set_cookie("user_name",'',max_age=-1)
+            # 登录成功保存session信息
+
+            request.session['user_id']=old_users[0].ZA_User_ID
+            request.session['user_name'] = old_users[0].ZA_User_Name
+            print("session信息保存成功！")
+            print("session——id:%s"%request.session['user_name'])
+            print("session——user_name:%s"%request.session['user_name'])
+
+            return JsonResponse({"login": "ok"}),cookiesWarn
+        else:
+
+            return JsonResponse({"login":"no"})
+
+
+
+
+
+        context = {'title': '用户成功！'}
+        return  render(request,'ZA_Show/ZA_show.html',context)
--- static/ZA_User/js/Login.js
+++ static/ZA_User/js/Login.js
@@ -1,10 +1,6 @@
-//easytip 错误提示
-// 调用方法：（样例）
-//     var tip = $("#inputUserId").easytip();
-//     tip.show("该用户名验证可用!")
-//
-//     在html相应id的标签中加入“data-easytip="position:top;class:easy-red;"”即可。
-
+/**
+ * Created by ruanqing on 17-9-4.
+ */
 (function ($, window, document, undefined)
 {
     var _easytip = function (ele, opt)
@@ -393,195 +389,53 @@ if (typeof(easy_load_options) == "undefined") {
     }
 }
 
+// 用户名验证
+function usernameCheck() {
+    // var
+    var _username = $("#inputUserId").val();
+    var _usernameLen = _username.length;
 
-var _user = false;
-// 用户名检测以及ajax
-function username() {
-    //var
-    var _username = $("#inputUserId");
-    var _usernameVal = _username.val();
-    var _usernameLen = _usernameVal.length;
-    var _usernameReg = /^([\w]|[\u4e00-\u9fa5]|[ 。，、？￥“”‘’！：【】《》（）——.,?!$'":+-]){4,16}$/;
-    // 判断是否符合前端正则
-    if (_usernameReg.test(_usernameVal)) {
-        // 符合前端验证，发送ajax请求进行后端验证
-        $.get('/user/register_ajax/?user_name='+_usernameVal,function (data){
-            if(data.count==0){
-                var tip = $("#inputUserId").easytip();
-                tip.show("该用户名验证可用!");
-                $(".logoAction1").attr("class","logoAction1 glyphicon glyphicon-ok form-control-feedback");
-                $(".uname").attr("class","form-group has-feedback uname has-success");
-                _user = true;
-                return _user;
-            }else if(data.count>0){
-
-                var tip = $("#inputUserId").easytip();
-                tip.show("该用户名已被使用！");
-                $(".uname").attr("class","form-group has-feedback uname has-error");
-                $(".logoAction1").attr("class","logoAction1 glyphicon glyphicon-remove form-control-feedback");
-                return false;
-            }
-        });
-    } else {
+    if (_usernameLen <= 0){
         var tip = $("#inputUserId").easytip();
-        $(".uname").addClass("has-error");
-        $(".logoAction1").attr("class","logoAction1 glyphicon glyphicon-remove form-control-feedback");
-
-        tip.show("用户名必须为4—16位的英文字母、数字或汉字！当前已经输入用户名的长度为："+_usernameLen+"。");
-        return false;
+        tip.show("好家伙，你居然不写用户名，这是让本站怎么登陆呢？");
+        return false
+    } else if (_usernameLen < 4){
+        var tip = $("#inputUserId").easytip();
+        tip.show("敢不敢用户名再长一点呐？");
+        return false
+    } else {
+        return true
     }
 }
 
-// 密码验证
+//密码验证
 function pwd() {
-    //var
-    var _pwd = $("#inputPassword1");
-    var _pwdVal = _pwd.val();
-    var _pwdLen = _pwdVal.length;
-    var _pwdReg = /^.*(?=.{6,})(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*? ]).*$/;
-    // console.log($(".has-feedback")[1]);
-
-    if (_pwdReg.test(_pwdVal)) {
-        $(".pwd1").attr("class","form-group has-feedback pwd1 has-success");
-        $(".logoAction2").attr("class","logoAction2 glyphicon glyphicon-ok form-control-feedback");
-        return true;
-    } else {
-        $(".pwd1").attr("class","form-group has-feedback pwd1 has-error");
-        $(".logoAction2").attr("class","logoAction2 glyphicon glyphicon-remove form-control-feedback");
-        if ( _pwdLen < 6) {
-            var tip = $("#inputPassword1").easytip();
-            tip.show("密码过于短小，要至少六位密码哦");
-        } else {
-            var tip = $("#inputPassword1").easytip();
-            tip.show("最少6位，包含至少1个大写字母，1个小写字母，1个数字，1个英文标点符号（比如英文“?”）！");
-        }
-        return false;
+    var _pwd = $("#inputPassword1").val();
+    var _pwdLen = _pwd.length;
+    if (_pwdLen <= 0){
+        var tip = $("#inputPassword1").easytip();
+        tip.show("朋友，你写一下密码好不好啦？");
+        return false
+    } else{
+        return true
     }
 }
 
-// 密码确认
-function pwdConfirm() {
-    //var
-    var _pwd1 = $("#inputPassword1");
-    var _pwd2 = $("#inputPassword2");
-    var _pwdVal1 = _pwd1.val();
-    var _pwdVal2 = _pwd2.val();
-    var _pwdLen = _pwdVal1.length;
-
-    if (_pwdVal1 === _pwdVal2  && _pwdLen >= 6 ) {
-        $(".pwd2").attr("class","form-group has-feedback pwd2 has-success");
-        $(".logoAction3").attr("class","logoAction3 glyphicon glyphicon-ok form-control-feedback");
-        return true;
-    } else {
-        $(".pwd2").attr("class","form-group has-feedback pwd2 has-error");
-        $(".logoAction3").attr("class","logoAction3 glyphicon glyphicon-remove form-control-feedback");
-        if (_pwdLen < 6){
-            var tip = $("#inputPassword2").easytip();
-            tip.show("密码太短了！");
-        } else {
-            var tip = $("#inputPassword2").easytip();
-            tip.show("密码两次输入得不相同欸！");
-        }
-        return false;
-    }
-}
-
-// 邮箱验证以及ajax验证
-var _email = false;
-function email() {
-    //var
-    var _useremail = $("#inputEmail1");
-    var _useremailVal = _useremail.val();
-    var _useremailReg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
-    // 判断是否符合前端正则
-    if (_useremailReg.test(_useremailVal)) {
-        // 符合前端验证，发送ajax请求进行后端验证
-        $.get('/user/register_ajax/?user_email='+_useremailVal,function (data){
-            if(data.count==0){
-                var tip = $("#inputEmail1").easytip();
-                tip.show("该电子邮箱验证可用!");
-                $(".logoAction4").attr("class","logoAction4 glyphicon glyphicon-ok form-control-feedback");
-                $(".email").attr("class","form-group has-feedback email has-success");
-                _email = true;
-                return _email;
-            }else if(data.count>0){
-
-                var tip = $("#inputEmail1").easytip();
-                tip.show("该邮箱已被使用过，同个邮箱不能重复注册！");
-                $(".email").attr("class","form-group has-feedback email has-error");
-                $(".logoAction4").attr("class","logoAction4 glyphicon glyphicon-remove form-control-feedback");
-                return false;
-            }
-        });
-    } else {
-        var tip = $("#inputEmail1").easytip();
-        $(".email").attr("class","form-group has-feedback email has-error");
-        $(".logoAction4").attr("class","logoAction4 glyphicon glyphicon-remove form-control-feedback");
-
-        tip.show("电子邮箱的格式好像不太对！");
-        return false;
-    }
-}
-
-
-// 手机号码验证以及ajax验证
-function phone() {
-    //var
-    var _userphone = $("#inputphone1");
-    var _userphoneVal = _userphone.val();
-    var _userphoneLen = _userphoneVal.length;
-    var _userphoneReg = /^1[34578]\d{9}$/;
-    // 判断是否符合前端正则
-    if (_userphoneLen !== 0){
-        if (_userphoneReg.test(_userphoneVal)) {
-            // 符合前端验证，发送ajax请求进行后端验证
-            $.get('/user/register_ajax/?user_phone='+_userphoneVal,function (data){
-                if(data.count==0){
-                    var tip = $("#inputphone1").easytip();
-                    tip.show("该号码验证可用!");
-                    $(".logoAction1").attr("class","logoAction1 glyphicon glyphicon-ok form-control-feedback");
-                    $(".phone").attr("class","form-group has-feedback phone has-success");
-                    return true;
-                }else if(data.count>0){
-
-                    var tip = $("#inputphone1").easytip();
-                    tip.show("该手机号已被注册！");
-                    $(".phone").attr("class","form-group has-feedback phone has-error");
-                    $(".logoAction5").attr("class","logoAction5 glyphicon glyphicon-remove form-control-feedback");
-                    return false;
-                }
-            });
-
-        } else {
-            var tip = $("#inputphone1").easytip();
-            $(".phone").attr("class","form-group has-feedback phone has-error");
-            $(".logoAction5").attr("class","logoAction5 glyphicon glyphicon-remove form-control-feedback");
-
-            tip.show("号码的格式好像不太对！");
-            return false;
-        }
-    } else {
-        return true;
-    }
-
-}
-
-var VC_res = false;
+//拼图验证
+var VC_ticket = null;
 window.callback = function(res){
     // res（未通过验证）= {ret: 1, ticket: null}
     // res（验证成功） = {ret: 0, ticket: "String", randstr: "String"}
     if(res.ret === 0){
         // alert(res.ticket); // 票据
-        VC_res = true;
-        return VC_res;
+        VC_ticket = res.ticket;
+        return VC_ticket;
     } else {
-        VC_res = false;
-        return VC_res;
+        return VC_ticket;
     }
 };
 
-
-//阅读协议勾选
+//记忆登录状态！
 function checkBox() {
     //var
     var checkBox = $("#inputcheckbox");
@@ -589,55 +443,62 @@ function checkBox() {
     if (checkBox.is(":checked")) {
         return true;
     } else {
-        var tip = $("#inputcheckbox").easytip();
-        tip.show("必须勾选同意协议才能注册哦");
         return false;
     }
 }
 
 
-// 前端认证汇总
-function register() {
-    // var _user = username();
-    var _pwd = pwd();
-    var _pwd2 = pwdConfirm();
-    // var _email = email();
+function login(){
+    // var
     var _checkBox = checkBox();
-    var _callback = VC_res;
-    var _phone = phone();
-    // alert("user:"+_user);
+    var _usernameCheck = usernameCheck();
+    var _pwd = pwd();
+    var _loginTicket = VC_ticket;
+    // alert("user:"+_usernameCheck);
     // alert("pwd:"+_pwd);
-    // alert("pwd2:"+_pwd2);
-    // alert("email:"+_email);
     // alert("checkBox:"+_checkBox);
-    // alert("callback:"+_callback);
-    // alert("phone:"+_phone);
-    //judge
-    if (_user === true && _pwd === true && _pwd2 === true && _email === true && _checkBox === true && _callback === true && _phone === true ){
-    // if (_user === true && _pwd === true && _pwd2 === true && _email === true && _checkBox === true && _phone === true ){
-        // var _usernameVal = $("#inputUserId").val();
-        // var _usernameVal = $("#inputPassword1").val();
-        // $.post(url, {
-        //     'user_name':_usernameVal,
-        //     'key2':val2
-        // });
-        return true;
+    // alert("VC_ticket:"+VC_ticket);
+
+    if (_loginTicket !== null && _usernameCheck === true && _pwd === true) {
+        var _usernameVal = $("#inputUserId").val();
+        var _pwdVal = $("#inputPassword1").val();
+        var _url = "192.168.0.103:5678/user/login_handle/";
+        // post提交验证，成功跳转
+        $.post(_url, {
+            headers: {'X-CSRFToken': this.getCookie('csrftoken')},
+            'user_name':_usernameVal,
+            'password':_pwdVal,
+            'loginTicket':_loginTicket.length,
+            'rememberme':_checkBox
+        },function (data){
+            if(data.login === "ok"){
+                alert("JQ有喜了！")
+            }else if(data.login === "no"){
+                var tip = $(".btn-login").easytip();
+                tip.show("用户名、邮箱或者密码不正确呢！");
+            }
+        });
+        return true
+
     } else {
-        var tip = $(".btn-register").easytip();
-        tip.show("注册信息不够完善！无法提交！请检查");
-        return false;
+        var tip = $(".btn-login").easytip();
+        tip.show("登陆失败，检查一下！");
+
     }
-
-
+    // 避免submit二次提交
+    return false
 }
 
-// 前端认证主函数
+function getCookie (name) {
+      var value = '; ' + document.cookie;
+      var parts = value.split('; ' + name + '=');
+      if (parts.length === 2) return parts.pop().split(';').shift();
+    }
+
+// 前端登陆主函数
 $(document).ready(function() {
-    $("#inputUserId").blur(username);
+    $("#inputUserId").blur(usernameCheck);
     $("#inputPassword1").blur(pwd);
-    $("#inputPassword2").blur(pwdConfirm);
-    $("#inputEmail1").blur(email);
-    $("#inputphone1").blur(phone);
     $("#inputcheckbox").blur(checkBox);
-    $(".btn-register").click(register);
+    $(".btn-login").click(login);
 });
