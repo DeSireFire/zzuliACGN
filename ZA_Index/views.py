2bdcfc7ce0550a8c1a63af372044e5803509a36a
DeSireFire <1025212779@qq.com>
2018-09-08 23:28:08 +0800

    git传不上去是什么情况！

--- ZA_Index/views.py
+++ ZA_Index/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 
-# 网站施工（维护）页
-def Construction_period(request):
-    return render(request,'ZA_Index/Construction_period.html')
+
 
 
