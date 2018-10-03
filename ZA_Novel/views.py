85b80aadc412455feddf2e258309ae8c97c03461
DeSireFire <1025212779@qq,com>
2018-10-03 22:25:43 +0800

    实装novel网页，删除部分文件

--- ZA_Novel/views.py
+++ ZA_Novel/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+# 网页渲染部分
+def novelsIndex(request):
+    context = {'title': '次元圣经'}
+    return render(request,'ZA_Novel/ZA_Novel.html',context)
