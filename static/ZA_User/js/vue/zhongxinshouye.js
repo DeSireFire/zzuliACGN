56d2e82e5f1dba534972ec5cf1902915c9c62ba7
xza <batqusi@gmail.com>
2018-11-09 20:28:26 +0800

    优化个人中心显示

--- static/ZA_User/js/vue/zhongxinshouye.js
+++ static/ZA_User/js/vue/zhongxinshouye.js
@@ -32,49 +32,57 @@ let zhongxinshouye = Vue.component('user-center-home-page',{
             <router-link class="btn btn-user btn-xs" to="/zhanghaoanquan"><i class="fa fa-fw fa-user-secret"></i> 更多设置 </router-link>
         </div>
         <div class="col-sm-6 col-md-6">
-            <div class="user-msg-th">
-                <img class="user-msg-th-icon" src="/static/ZA_User/img/background/邮箱.png"/>
-                <div class="user-msg-th-cont">
-                    <b class="h-safe-title">我的邮箱</b>
-                    <p class="h-safe-desc">绑定邮箱即可邮箱登录</p>
-                    <a class="btn btn-user btn-xs disabled" role="button">{{userbasedata.email}}</a>
-                    <router-link to="/zhanghaoanquan" class="btn btn-user btn-xs">更改邮箱 </router-link>
-                </div>
+        <div class="user-msg-th">
+            <img class="user-msg-th-icon" src="Images/邮箱.png"/>
+            <div class="user-msg-th-cont">
+                <b class="h-safe-title">我的邮箱</b>
+                <p class="h-safe-desc">绑定邮箱即可邮箱登录</p>
+                <a class="btn btn-user btn-xs disabled" role="button" v-if=userdata.emailif>已认证</a>
+                <a class="btn btn-user btn-xs disabled" role="button" v-if=!userdata.emailif>未认证</a>
+                <router-link to="/zhanghaoanquan" class="btn btn-user btn-xs" v-if=userdata.emailif>更改邮箱 </router-link>
+                <router-link to="/zhanghaoanquan" class="btn btn-user btn-xs" v-if=!userdata.emailif>绑定邮箱 </router-link>
             </div>
         </div>
-        <div class="col-sm-6 col-md-6">
-            <div class="user-msg-th">
-                <img class="user-msg-th-icon" src="/static/ZA_User/img/background/手机.png"/>
-                <div class="user-msg-th-cont">
-                    <b class="h-safe-title">我的手机</b>
-                    <p class="h-safe-desc">绑定手机即可手机登录</p>
-                    <a class="btn btn-user btn-xs disabled" role="button">{{userbasedata.phone}}</a>
-                    <router-link to="/zhanghaoanquan" class="btn btn-user btn-xs">更改手机 </router-link>
-                </div>
+    </div>
+    <div class="col-sm-6 col-md-6">
+        <div class="user-msg-th">
+            <img class="user-msg-th-icon" src="Images/手机.png"/>
+            <div class="user-msg-th-cont">
+                <b class="h-safe-title">我的手机</b>
+                <p class="h-safe-desc">绑定手机即可手机登录</p>
+                <a class="btn btn-user btn-xs disabled" role="button" v-if=userdata.phoneif>已认证</a>
+                <a class="btn btn-user btn-xs disabled" role="button" v-if=!userdata.phoneif>未认证</a>
+                <router-link to="/zhanghaoanquan" class="btn btn-user btn-xs" v-if=userdata.phoneif>更改手机 </router-link>
+                <router-link to="/zhanghaoanquan" class="btn btn-user btn-xs" v-if=!userdata.phoneif>绑定手机 </router-link>
             </div>
         </div>
-        <div class="col-sm-6 col-md-6">
-            <div class="user-msg-th">
-                <img class="user-msg-th-icon" src="/static/ZA_User/img/background/写报告.png"/>
-                <div class="user-msg-th-cont">
-                    <b class="h-safe-title">密保问题</b>
-                    <p class="h-safe-desc">设置密保，账号更安全</p>
-                    <a class="btn btn-user btn-xs disabled" role="button">{{userbasedata.question}}</a>
-                    <router-link to="/zhanghaoanquan" class="btn btn-user btn-xs">设置密保 </router-link>
-                </div>
+    </div>
+    <div class="col-sm-6 col-md-6">
+        <div class="user-msg-th">
+            <img class="user-msg-th-icon" src="Images/写报告.png"/>
+            <div class="user-msg-th-cont">
+                <b class="h-safe-title">密保问题</b>
+                <p class="h-safe-desc">设置密保，账号更安全</p>
+                <a class="btn btn-user btn-xs disabled" role="button" v-if=userdata.questionif>已设置</a>
+                <a class="btn btn-user btn-xs disabled" role="button" v-if=!userdata.questionif>未设置</a>
+                <router-link to="/zhanghaoanquan" class="btn btn-user btn-xs" v-if=userdata.questionif>更改密保 </router-link>
+                <router-link to="/zhanghaoanquan" class="btn btn-user btn-xs" v-if=!userdata.questionif>设置手机 </router-link>
             </div>
         </div>
-        <div class="col-sm-6 col-md-6">
-            <div class="user-msg-th">
-                <img class="user-msg-th-icon" src="/static/ZA_User/img/background/代办事项.png"/>
-                <div class="user-msg-th-cont">
-                    <b class="h-safe-title">实名认证</b>
-                    <p class="h-safe-desc">学号认证、证件认证</p>
-                    <a class="btn btn-user btn-xs disabled" role="button">{{userbasedata.certification}}</a>
-                    <router-link to="/shimingrenzheng" class="btn btn-user btn-xs">实名认证 </router-link>
-                </div>
+    </div>
+    <div class="col-sm-6 col-md-6">
+        <div class="user-msg-th">
+            <img class="user-msg-th-icon" src="Images/代办事项.png"/>
+            <div class="user-msg-th-cont">
+                <b class="h-safe-title">实名认证</b>
+                <p class="h-safe-desc">学号认证、证件认证</p>
+                <a class="btn btn-user btn-xs disabled" role="button" v-if=userdata.certificationif>已认证</a>
+                <a class="btn btn-user btn-xs disabled" role="button" v-if=!userdata.certificationif>未认证</a>
+                <router-link to="/zhanghaoanquan" class="btn btn-user btn-xs" v-if=userdata.certificationif>查看认证 </router-link>
+                <router-link to="/zhanghaoanquan" class="btn btn-user btn-xs" v-if=!userdata.certificationif>进行认证 </router-link>
             </div>
         </div>
+    </div>
         <!--<div class="col-md-6" style="background-color: #00FFCC">-->
         <!---->
         <!--</div>-->
