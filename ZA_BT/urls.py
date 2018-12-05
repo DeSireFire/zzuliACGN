8904f02e2ed62b074fdc2317bbe011470400c1d1
DeSireFire <1025212779@qq.com>
2018-12-05 17:54:44 +0800

    后端简单的响应跳转

--- ZA_BT/urls.py
+++ ZA_BT/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 
 urlpatterns = [
-    path('',views.novelsIndex),
+    path('',views.BTIndex),
 ]
--- ZA_BT/views.py
+++ ZA_BT/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+# 网页渲染部分
+def BTIndex(request):
+    context = {'title': '资源下载'}
+    return render(request,'ZA_ResourceDownload/Resourcedownload.html',context)
