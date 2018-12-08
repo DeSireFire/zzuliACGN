df337b886bb4985b716d05222edc612c278f708b
DeSireFire <1025212779@qq.com>
2018-12-08 16:18:47 +0800

    修整BT模型，注册admin

--- ZA_BT/admin.py
+++ ZA_BT/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+class R_typeAdmin(admin.ModelAdmin):
+    list_display = ['id','R_title']
+class R_infoAdmin(admin.ModelAdmin):
+    '''
+    用户查询在admin界面中的显示
+    '''
+    list_per_page = 30
+    list_display = [
+    'rdName',
+    'rdUpTime',
+    'rdSize',
+    'rdUpNum',
+    'rdDownloadNum',
+    'rdInfo',
+    'rdOK',
+    'rdURLS',
+    'rdType',
+    'isdelete',
+    ]
+    # list_display = [
+    #     'a_i_u_e_o',
+    #     'ka_ki_ku_ke_ko',
+    #     'sa_shi_su_se_so',
+    #     'ta_chi_tsu_te_to',
+    #     'na_ni_nu_ne_no',
+    #     'ha_hi_fu_he_ho',
+    #     'ma_mi_mu_me_mo',
+    #     'ya_yu_yo',
+    #     'ra_ri_ru_le_ro',
+    #     'wa_n_wo',
+    # ]
+# # 在admin页面里面注册model中的类和admin.py中对应的类
+admin.site.register(R_type,R_typeAdmin)
+admin.site.register(R_info,R_infoAdmin)
--- ZA_BT/models.py
+++ ZA_BT/models.py
@@ -11,15 +11,15 @@ class R_type(models.Model):
 
 #资源信息
 class R_info(models.Model):
-    a_i_u_e_o = models.CharField(max_length=50,unique=True)#资源名称
-    ka_ki_ku_ke_ko = models.CharField(max_length=30,unique=True)  # 资源发布时间
-    sa_shi_su_se_so = models.CharField(max_length=30,default='-')#资源大小
-    ta_chi_tsu_te_to = models.CharField(max_length=50,default='-')#资源上传数
-    na_ni_nu_ne_no = models.CharField(max_length=50,default='-')#资源下载数
-    ha_hi_fu_he_ho = models.TextField(default='暂无')#资源介绍
-    ma_mi_mu_me_mo = models.CharField(max_length=50,default='-')#资源完成数
-    ya_yu_yo = models.TextField(default='暂无')    #资源下载链接
-    ra_ri_ru_le_ro = models.ForeignKey('R_type',on_delete=models.CASCADE)#资源种类
-    wa_n_wo = models.BooleanField(default=False)#是否删除
+    rdName = models.CharField(max_length=50,unique=True)#资源名称
+    rdUpTime = models.CharField(max_length=30,unique=True)  # 资源发布时间
+    rdSize = models.CharField(max_length=30,default='-')#资源大小
+    rdUpNum = models.CharField(max_length=50,default='-')#资源上传数
+    rdDownloadNum = models.CharField(max_length=50,default='-')#资源下载数
+    rdInfo = models.TextField(default='暂无')#资源介绍
+    rdOK = models.CharField(max_length=50,default='-')#资源完成数
+    rdURLS = models.TextField(default='暂无')    #资源下载链接
+    rdType = models.ForeignKey('R_type',on_delete=models.CASCADE)#资源种类
+    isdelete = models.BooleanField(default=False)#是否删除
     def __str__(self):
-        return self.a_i_u_e_o
+        return self.rdName
