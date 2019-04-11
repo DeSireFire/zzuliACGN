acc385c0a1cdb91937c04a89ddbd2d4cb3cdb063
xuziang111 <584788096@qq.com>
2019-04-11 11:18:19 +0800

    文章页面路由问题

--- static/ZA_News/JavaScript/article.js
+++ static/ZA_News/JavaScript/article.js
@@ -31,6 +31,14 @@ let articles = Vue.component('articles',{
           this.content = data
         }
     },
+    beforeRouteUpdate (to, from, next) {
+        // 在当前路由改变，但是该组件被复用时调用
+        // 举例来说，对于一个带有动态参数的路径 /foo/:id，在 /foo/1 和 /foo/2 之间跳转的时候，
+        // 由于会渲染同样的 Foo 组件，因此组件实例会被复用。而这个钩子就会在这个情况下被调用。
+        // 可以访问组件实例 `this`
+        console.log(to.params.id)
+        this.getArticle({page:'article',part:to.params.id})
+      },
     created:function(){
         this.getArticle({page:'article',part:this.id})
         //{page:'article',part:this.id}
