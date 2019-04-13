b9e495fe593a93a2ce8c276fd3c5c1ed13d10f6b
DeSireFire <1025212779@qq.com>
2019-04-14 01:24:35 +0800

    小说类别的动态加载

--- ZA_Novel/views.py
+++ ZA_Novel/views.py
@@ -3,10 +3,14 @@ from .models import *
 # Create your views here.
 # 网页渲染部分
 def novelsIndex(request):
-    Ntype = type.objects.select_related().all().order_by("id").exclude(isdelete=1)
-    # print(list(Ntype.values_list('Type_title', flat=True)))
     context = {
         'title': '次元圣经',
-        'novelTypes':list(Ntype.values_list('Type_title', flat=True))
+        'novelTypes':[]
     }
+    NtypeAll = Ntype.objects.select_related().all().order_by("id").exclude(isdelete=1)
+    NtypeAll_list = list(NtypeAll.values_list('Type_title', flat=True))
+    for a, b in zip(NtypeAll_list, [i for i in NtypeAll_list if i not in NtypeAll_list[1::2]]):
+        context['novelTypes'].append('<li><a href="?id=%s">%s</a><a href="?id=%s">%s</a></li>'%(NtypeAll_list.index(a),a,NtypeAll_list.index(b),b))
+    if not context['novelTypes']:
+        context['novelTypes'] = ['<li><a href="#">暂无</a></li>']
     return render(request,'ZA_Novel/ZA_Novel.html',context)
