a0c54db3dcfa0d46a5d4498389235b795f4872de
DeSireFire <1025212779@qq.com>
2019-04-14 00:36:12 +0800

    修改图库模型

--- ZA_Tools/models.py
+++ ZA_Tools/models.py
@@ -4,9 +4,9 @@ from django.db import models
 
 # 图库整合
 class Gallerys(models.Model):
-    imgName = models.TextField('图片文件名')
+    imgName = models.CharField('图片文件名',unique=True,max_length=255)
     axxUrl = models.URLField('爱信息图片地质',default='')
     smUrl = models.URLField('sm图片地质',default='')
-    axxKey = models.CharField('爱信息图Key',default='',unique=True)
-    smhash = models.CharField('sm图hash',default='',unique=True)
+    axxKey = models.CharField('爱信息图Key',default='',max_length=255)
+    smhash = models.CharField('sm图hash',default='',max_length=255)
     imgfrom = models.CharField('所属应用',default='图库')# 按照django应用名来划分，例如：user（代表头像）、new(代表咨询图片)、Gallery(代表图库)、novel(代表小说封面或者插图)
