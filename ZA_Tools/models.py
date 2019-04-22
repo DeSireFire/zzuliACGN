2da1f4467929fb51e162be26daece405684ae912
DeSireFire <1025212779@qq.com>
2019-04-23 01:09:10 +0800

    修改tool应用中的图片表字段

--- ZA_Tools/models.py
+++ ZA_Tools/models.py
@@ -5,8 +5,9 @@ from django.db import models
 # 图库整合
 class Gallerys(models.Model):
     imgName = models.CharField('图片文件名',unique=True,max_length=255)
-    axxUrl = models.URLField('爱信息图片地质',default='')
-    smUrl = models.URLField('sm图片地质',default='')
-    axxKey = models.CharField('爱信息图Key',default='',max_length=255)
-    smhash = models.CharField('sm图hash',default='',max_length=255)
+    url = models.URLField('图片地址',default='')
+    origin = models.URLField('原图片地址',default='')
+    hash = models.CharField('图hash或者key',default='',max_length=255)
+    imgMd5 = models.CharField('通过hash字段加密的md5字符串，用于其他表调用',default='',max_length=255)
+    bedName = models.CharField('托管的图床名',default='',max_length=255)
     imgfrom = models.CharField('所属应用',max_length=255,default='图库')# 按照django应用名来划分，例如：user（代表头像）、new(代表咨询图片)、Gallery(代表图库)、novel(代表小说封面或者插图)
