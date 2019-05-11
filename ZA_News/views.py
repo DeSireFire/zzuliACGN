112a611c53027da88f166d8f2ca6d57f9a7f09ef
DeSireFire <1025212779@qq.com>
2019-05-12 00:10:44 +0800

    调整小说页前端代码

--- ZA_News/views.py
+++ ZA_News/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 # 网站模块临时展示页
 def ZA_Show(request):
-    context = {'title': '社团招新'}
+    context = {'title': '宅新闻'}
     return render(request,'ZA_News/index.html',context)
